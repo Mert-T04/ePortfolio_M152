9547a4db60434585d7d5f99df0169a5170fade05
Mert-T04 <95247743+Mert-T04@users.noreply.github.com>
2023-12-22 03:34:26 +0100

    feat: added some images

--- src/routes/images/+page.svelte
+++ src/routes/images/+page.svelte
@@ -8,63 +8,57 @@
 	export const images1 = [
 		{
 			alt: 'FPV Drone',
-			src: '/2023/ina4a/gruppe20/droneOriginal.jpeg',
-			title: 'FPV Drone'
+			src: '/2023/ina4a/gruppe20/droneOriginal.jpg',
 		},
 		{
 			alt: 'FPV Drone',
-			src: '/2023/ina4a/gruppe20/droneEdited.png',
-			title: 'FPV Drone'
+			src: '/2023/ina4a/gruppe20/droneEdited.jpg',
 		}
 	]
 	export const images2 = [
 		{
-			alt: 'ping',
+			alt: 'Fidget Drone',
 			src: '/2023/ina4a/gruppe20/propsOriginal.jpg',
-			title: 'ping'
 		},
 		{
-			alt: 'pong',
+			alt: 'Fidget Drone',
 			src: '/2023/ina4a/gruppe20/propsEdited.jpg',
-			title: 'pong'
 		}
 	]
 	export const images3 = [
 		{
-			alt: 'hello',
-			src: 'https://picsum.photos/1280/720?random=1',
-			title: 'hello'
+			alt: 'Liftoff',
+			src: '/2023/ina4a/gruppe20/liftoffOriginal.jpg',
 		},
 		{
-			alt: 'world',
-			src: 'https://picsum.photos/1280/720?random=2',
-			title: 'world'
+			alt: 'Liftoff',
+			src: '/2023/ina4a/gruppe20/liftoffEdited.jpg',
 		}
 	]
 </script>
 
-<div class="flex items-center align-center flex-col">
+<div class="flex flex-col">
 	<Heading tag="h1" class="text-center mt-16">Bildergallerie</Heading>
 
 	<p class="my-32 text-lg text-center dark:text-slate-100">
 		Jedes Bild erzählt eine eigene Geschichte und demonstriert die beeindruckenden Möglichkeiten der FPV-Technologie. Durch Bildbearbeitung bekommt jede Aufnahme einen speziellen Touch.
 	</p>
 	
-	<div class="w-full aspect-video max-w-4xl my-16">
+	<div class="max-w-4xl my-16">
 		<Carousel images={images1} transition={scaleAnimation} let:Controls let:Indicators>
 			<Controls />
 			<Indicators />
 		</Carousel>
 	</div>
 	
-	<div class="w-full aspect-video max-w-4xl my-16">
+	<div class="max-w-4xl my-16">
 		<Carousel images={images2} transition={scaleAnimation} let:Controls let:Indicators>
 			<Controls />
 			<Indicators />
 		</Carousel>
 	</div>
 	
-	<div class="w-full aspect-video max-w-4xl my-16">
+	<div class="max-w-4xl my-16">
 		<Carousel images={images3} transition={scaleAnimation} let:Controls let:Indicators>
 			<Controls />
 			<Indicators />
--- src/routes/setup/+page.svelte
+++ src/routes/setup/+page.svelte
@@ -1,7 +1,34 @@
 <script>
-	import { Heading, Skeleton } from 'flowbite-svelte';
+	import { Card, Heading } from 'flowbite-svelte';
 </script>
-<Heading tag="h1">Setup Page not implemented yet</Heading>
-<Skeleton size="sm" class="my-8" />
-<Skeleton size="sm" class="my-8" />
-<Skeleton size="sm" class="my-8" />
+<div class="flex flex-wrap justify-center gap-8">
+	<Heading tag="h1" class="text-center mt-16">Setup</Heading>
+
+	<p class="my-32 text-lg text-center dark:text-slate-100">
+		Zum FPV-Drohnen Hobby gehört nicht nur die Drohne alleine. Man braucht auch weiteres an Equipment, damit man problemlos fliegen kann.
+	</p>
+	<Card img="/2023/ina4a/gruppe20/setup/drone.jpg" class="mb-4">
+		<h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Drohne</h3>
+		<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Viele Enthusiasten bauen ihre Drohne selber zusammen. Dazu kaufen sie die Einzelnen Komponenten wie Motoren, Flight Controller, Antennen etc. um alles selber zusammenzubauen und zusammenzulöten. Ich habe meine Drohne von iFly gekauft, möchte jedoch meine nächste Drohne definitiv selber bauen.</p>
+	</Card>
+	<Card img="/2023/ina4a/gruppe20/setup/goggles.jpg" class="mb-4">
+		<h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Goggles</h3>
+		<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Wie der Name schon sagt, sieht man beim FPV (First Person View) aus der Sicht der Drohne. Um die Sicht immer zu behalten und nicht gestört zu werden, setzt man eine Brille auf in der man die Live-¨Ubertragung der Drohnenkamera sieht. Ich benutze die FatShark Scout.</p>
+	</Card>
+	<Card img="/2023/ina4a/gruppe20/setup/radio.jpg" class="mb-4">
+		<h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Radio</h3>
+		<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Mit dem Radio bzw. dem controller kontrolliert man die Drohne an sich. Es ähnelt sehr einem Spielecontroller für eine Konsole, funktioniert jedoch etwas anders. Die RadioMaster Zorro ist eine sehr beliebte Wahl in dem Hobby.</p>
+	</Card>
+	<Card img="/2023/ina4a/gruppe20/setup/gopro.jpg" class="mb-4">
+		<h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">GoPro</h3>
+		<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Um im Flug gute Aufnahmen zu erzielen, habe ich mir vor kurzem eine GoPro Hero 11 gekauft. Um das Video nicht zu überbelichten, werden oft sogenannte ND-Filter an die Linse befestigt.</p>
+	</Card>
+	<Card img="/2023/ina4a/gruppe20/setup/lipos.jpg" class="mb-4">
+		<h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">LiPo-Batterie</h3>
+		<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">In FPV-Drohnen und auch bei anderen RC-Geräten werden sehr oft Lithium-Polymer-Akkus verwendet. LiPos können viel mehr Energie auf einmal ausgeben als gewöhnliche Lithium-Ionen-Akkus, weshalb sie ideal für stromfressende Motoren in einer Drohne sind. Ich habe all meine Akkus von Tattu gekauft.</p>
+	</Card>
+	<Card img="/2023/ina4a/gruppe20/setup/charger.jpg" class="mb-4">
+		<h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Charger</h3>
+		<p class="mb-3 font-normal text-gray-700 dark:text-gray-400 leading-tight">Die LiPo-Akkus müssen auch aufgeladen werden. Dazu wird ein spezielles Ladegerät verwendet, mit der man den Stromzufur genau anpassen kann. Da man mit LiPo-Akkus vorsichtig umgehen soll und auf keinen Fall falsch aufladen darf, habe ich mir ein hochwertigeres Ladegerät geholt.</p>
+	</Card>
+</div>
